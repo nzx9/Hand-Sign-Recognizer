d688eae46f4ec82c681c7637775ec6edd8e8cb24
Navindu Dananga <navindudananga123@gmail.com>
2021-10-17 14:47:27 +0530

    removed cnn_net1 file and src/trainer file

--- src/trainer.py
+++
@@ -1,8 +0,0 @@
-class ASL_RGB_Trainer:
-    def __init__(self, batch_size, learning_rate):
-        self.batch_size = batch_size
-        self.learning_rate = learning_rate
-
-
-def train(net, dataloader, loss_func, optimizer, epochs):
-    pass
